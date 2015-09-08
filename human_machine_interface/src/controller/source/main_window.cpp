80a8520d9a6a4d6823e627d7a2b46c01530b6d23
yolanda <yolanda93h@gmail.com>
2015-09-08 11:03:31 +0200

    Fixed OpenSceneGraph bug in laptops!

--- human_machine_interface/src/controller/source/main_window.cpp
+++ human_machine_interface/src/controller/source/main_window.cpp
@@ -151,8 +151,8 @@ void MainWindow::show_frame()
     max_osg_frame++;*/
 
     // Get the frame and visualize with a pixmap in a QLabel.
-    ui->sphereScene->setPixmap(osg_sphere->renderPixmap(0,0,true));
-    ui->vehicleScene->setPixmap(osg_uav->renderPixmap(0,0,true));
+    ui->sphereScene->setPixmap(osg_sphere->renderPixmap(0,0,false));
+    ui->vehicleScene->setPixmap(osg_uav->renderPixmap(0,0,false));
 
 }
 
@@ -265,9 +265,9 @@ void MainWindow::showNoMasterMessage()
 
 void MainWindow::flightTime()
 {
-    this->current_time->setHMS(this->current_time->addSecs(+1).hour(),this->current_time->addSecs(+1).minute(),this->current_time->addSecs(+1).second());
+    /*this->current_time->setHMS(this->current_time->addSecs(+1).hour(),this->current_time->addSecs(+1).minute(),this->current_time->addSecs(+1).second());
     QString text = this->current_time->toString();
-    ui->value_FligthTime->setText(text);
+    ui->value_FligthTime->setText(text);*/
 }
 
 
